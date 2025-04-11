26e8af5ef74e5eceb76d185c8d39bea532c53a2f
MichaelDenmead <michael@cthinking.co.uk>
2025-04-11 06:14:33 +0100

    Added login link to footer and other tweaks

--- sql/02-create-tables.sql
+++ sql/02-create-tables.sql
@@ -6,30 +6,6 @@ CREATE TABLE hotelbooking.staff (
     full_name TEXT
 );
 
-SELECT * FROM hotelbooking.staff;
-SELECT * FROM hotelbooking.booking ORDER BY b_ref DESC;
-
-SELECT 
-  b.*, 
-  c.c_name, 
-  c.c_email
-FROM 
-  hotelbooking.booking b
-JOIN 
-  hotelbooking.customer c ON b.c_no = c.c_no
-ORDER BY 
-  b.b_ref DESC;
-
-
-ALTER TABLE hotelbooking.staff
-ADD COLUMN role TEXT CHECK (role IN ('receptionist', 'housekeeper')) NOT NULL DEFAULT 'receptionist';
-
-UPDATE hotelbooking.staff
-SET role = 'housekeeper'
-WHERE username = 'tester';
-
-SELECT username, role FROM hotelbooking.staff;
-
 
 -- Create table: customer
 CREATE TABLE hotelbooking.customer (
